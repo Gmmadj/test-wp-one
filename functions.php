3e227043e85b57bcb70ae5a71477744a0541fb16
Gmmadj <56927849+Gmmadj@users.noreply.github.com>
2020-07-26 21:06:09 +0400

    Добавил виджет sidebar
    Поменял в style.css силекторы (этого можно было и не делать).
    Естественно добавил виджет в правую область и зарегал его.
    Переопределил Walker_Category, менял $link = sprintf( ...
    И удалил часть, которая цифры добавляет в скобках

--- functions.php
+++ functions.php
@@ -289,3 +289,192 @@ function test_wp_one_customize_register( $wp_customize )
 
 }
 add_action('customize_register', 'test_wp_one_customize_register');
+
+/**
+ * Add a sidebar
+ */
+function test_wp_one_widgets_init()
+{
+	/**
+	 * Creates a sidebar
+	 * @param string|array  Builds Sidebar based off of 'name' and 'id' values.
+	 */
+	$args = array(
+		'name'          => __( 'Main Sidebar', 'test_wp_one' ),
+		'id'            => 'sidebar-1',
+		'description'   => 'One sidebar',
+		'before_widget' => '<div id="%1$s" class="sidebar_wrap widget_categories">',
+		'after_widget'  => '</div>',
+		'before_title'  => '<div class="side_bar_heading">
+								<h6>',
+		'after_title'   => '</h6>							
+							</div>',
+	);
+	
+	register_sidebar( $args );
+	
+}
+
+add_action( 'widgets_init', 'test_wp_one_widgets_init' );
+
+function test_wp_one_widget_categories($args)
+{
+	$walker = new Walker_Categories_test_wp_one();
+	$args = array_merge($args, ['walker' => $walker]);
+
+	return $args;
+}
+
+add_filter('widget_categories_args', 'test_wp_one_widget_categories');
+
+class Walker_Categories_test_wp_one extends Walker_Category
+{
+	public function start_el( &$output, $category, $depth = 0, $args = array(), $id = 0 ) {
+		/** This filter is documented in wp-includes/category-template.php */
+		$cat_name = apply_filters( 'list_cats', esc_attr( $category->name ), $category );
+
+		// Don't generate an element if the category name is empty.
+		if ( '' === $cat_name ) {
+			return;
+		}
+
+		$atts         = array();
+		$atts['href'] = get_term_link( $category );
+
+		if ( $args['use_desc_for_title'] && ! empty( $category->description ) ) {
+			/**
+			 * Filters the category description for display.
+			 *
+			 * @since 1.2.0
+			 *
+			 * @param string $description Category description.
+			 * @param object $category    Category object.
+			 */
+			$atts['title'] = strip_tags( apply_filters( 'category_description', $category->description, $category ) );
+		}
+
+		/**
+		 * Filters the HTML attributes applied to a category list item's anchor element.
+		 *
+		 * @since 5.2.0
+		 *
+		 * @param array   $atts {
+		 *     The HTML attributes applied to the list item's `<a>` element, empty strings are ignored.
+		 *
+		 *     @type string $href  The href attribute.
+		 *     @type string $title The title attribute.
+		 * }
+		 * @param WP_Term $category Term data object.
+		 * @param int     $depth    Depth of category, used for padding.
+		 * @param array   $args     An array of arguments.
+		 * @param int     $id       ID of the current category.
+		 */
+		$atts = apply_filters( 'category_list_link_attributes', $atts, $category, $depth, $args, $id );
+
+		$attributes = '';
+		foreach ( $atts as $attr => $value ) {
+			if ( is_scalar( $value ) && '' !== $value && false !== $value ) {
+				$value       = ( 'href' === $attr ) ? esc_url( $value ) : esc_attr( $value );
+				$attributes .= ' ' . $attr . '="' . $value . '"';
+			}
+		}
+		$cat_number .= '<span>' . number_format_i18n( $category->count ) . '</span>';
+
+		$link = sprintf(
+			'<a%s><i class="fa fa-folder-open-o" aria-hidden="true"></i>%s%s</a>',
+			$attributes,
+			$cat_name,
+			$cat_number,
+		);
+
+		if ( ! empty( $args['feed_image'] ) || ! empty( $args['feed'] ) ) {
+			$link .= ' ';
+
+			if ( empty( $args['feed_image'] ) ) {
+				$link .= '(';
+			}
+
+			$link .= '<a href="' . esc_url( get_term_feed_link( $category->term_id, $category->taxonomy, $args['feed_type'] ) ) . '"';
+
+			if ( empty( $args['feed'] ) ) {
+				/* translators: %s: Category name. */
+				$alt = ' alt="' . sprintf( __( 'Feed for all posts filed under %s' ), $cat_name ) . '"';
+			} else {
+				$alt   = ' alt="' . $args['feed'] . '"';
+				$name  = $args['feed'];
+				$link .= empty( $args['title'] ) ? '' : $args['title'];
+			}
+
+			$link .= '>';
+
+			if ( empty( $args['feed_image'] ) ) {
+				$link .= $name;
+			} else {
+				$link .= "<img src='" . esc_url( $args['feed_image'] ) . "'$alt" . ' />';
+			}
+			$link .= '</a>';
+
+			if ( empty( $args['feed_image'] ) ) {
+				$link .= ')';
+			}
+		}
+
+
+		if ( 'list' == $args['style'] ) {
+			$output     .= "\t<li";
+			$css_classes = array(
+				'cat-item',
+				'cat-item-' . $category->term_id,
+			);
+
+			if ( ! empty( $args['current_category'] ) ) {
+				// 'current_category' can be an array, so we use `get_terms()`.
+				$_current_terms = get_terms(
+					array(
+						'taxonomy'   => $category->taxonomy,
+						'include'    => $args['current_category'],
+						'hide_empty' => false,
+					)
+				);
+
+				foreach ( $_current_terms as $_current_term ) {
+					if ( $category->term_id == $_current_term->term_id ) {
+						$css_classes[] = 'current-cat';
+						$link          = str_replace( '<a', '<a aria-current="page"', $link );
+					} elseif ( $category->term_id == $_current_term->parent ) {
+						$css_classes[] = 'current-cat-parent';
+					}
+					while ( $_current_term->parent ) {
+						if ( $category->term_id == $_current_term->parent ) {
+							$css_classes[] = 'current-cat-ancestor';
+							break;
+						}
+						$_current_term = get_term( $_current_term->parent, $category->taxonomy );
+					}
+				}
+			}
+
+			/**
+			 * Filters the list of CSS classes to include with each category in the list.
+			 *
+			 * @since 4.2.0
+			 *
+			 * @see wp_list_categories()
+			 *
+			 * @param array  $css_classes An array of CSS classes to be applied to each list item.
+			 * @param object $category    Category data object.
+			 * @param int    $depth       Depth of page, used for padding.
+			 * @param array  $args        An array of wp_list_categories() arguments.
+			 */
+			$css_classes = implode( ' ', apply_filters( 'category_css_class', $css_classes, $category, $depth, $args ) );
+			$css_classes = $css_classes ? ' class="' . esc_attr( $css_classes ) . '"' : '';
+
+			$output .= $css_classes;
+			$output .= ">$link\n";
+		} elseif ( isset( $args['separator'] ) ) {
+			$output .= "\t$link" . $args['separator'] . "\n";
+		} else {
+			$output .= "\t$link<br />\n";
+		}
+	}
+}
